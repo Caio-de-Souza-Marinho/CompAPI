05aa40e752e3ff390ee615bee7c41f34595cde35
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-06-12 02:39:12 -0300

    Formatand código

--- Controllers/ParticipantesController.cs
+++ Controllers/ParticipantesController.cs
@@ -17,51 +17,51 @@ namespace CompAPI.Controllers
 
     public class ParticipantesController : ControllerBase
     {
-       private readonly IConfiguration _config;
+        private readonly IConfiguration _config;
 
-       public ParticipantesController(IConfiguration config)
-       {
-           _config = config;
-       }  
-
-    [HttpGet]
-    public async Task<IActionResult> GetAllAsync()
-    {
-        using (IDbConnection conexao = ConnectionFactory.GetStringConexao(_config))
+        public ParticipantesController(IConfiguration config)
         {
-            conexao.Open();
-
-            StringBuilder sql = new StringBuilder();
-            sql.Append("SELECT ID as Id, ID_TIPO_PARTICIPANTE as TipoId, TX_NOME as Nome, ");
-            sql.Append("TX_CPF as Cpf, TX_EMAIL as Email ");
-            sql.Append("FROM TB_PARTICIPANTE ");
-
-            List<Participante> lista = (await conexao.QueryAsync<Participante>(sql.ToString())).ToList();
-
-            return Ok(lista);
+            _config = config;
         }
-    }
- 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetByIdAsync(int id)
-    {
-        Participante p = null;
-        using (IDbConnection conexao = ConnectionFactory.GetStringConexao(_config))
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
         {
-            conexao.Open();
+            using (IDbConnection conexao = ConnectionFactory.GetStringConexao(_config))
+            {
+                conexao.Open();
 
-            StringBuilder sql = new StringBuilder();
-            sql.Append("SELECT ID as Id, ID_TIPO_PARTICIPANTE as TipoId, TX_NOME as Nome, TX_CPF as Cpf , ");
-            sql.Append("TX_EMAIL as Email FROM TB_PARTICIPANTE WHERE ID = @Id ");
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT ID as Id, ID_TIPO_PARTICIPANTE as TipoId, TX_NOME as Nome, ");
+                sql.Append("TX_CPF as Cpf, TX_EMAIL as Email ");
+                sql.Append("FROM TB_PARTICIPANTE ");
 
-            p = await conexao.QueryFirstOrDefaultAsync<Participante>(sql.ToString(), new {Id = id});
+                List<Participante> lista = (await conexao.QueryAsync<Participante>(sql.ToString())).ToList();
 
-            if (p != null)
-                return Ok(p);
-            else
-                return NotFound("Participante não encontrado");
+                return Ok(lista);
+            }
         }
-    }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            Participante p = null;
+            using (IDbConnection conexao = ConnectionFactory.GetStringConexao(_config))
+            {
+                conexao.Open();
+
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT ID as Id, ID_TIPO_PARTICIPANTE as TipoId, TX_NOME as Nome, TX_CPF as Cpf , ");
+                sql.Append("TX_EMAIL as Email FROM TB_PARTICIPANTE WHERE ID = @Id ");
+
+                p = await conexao.QueryFirstOrDefaultAsync<Participante>(sql.ToString(), new { Id = id });
+
+                if (p != null)
+                    return Ok(p);
+                else
+                    return NotFound("Participante não encontrado");
+            }
+        }
 
 
 
