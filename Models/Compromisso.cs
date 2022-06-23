c7d78e2207e2f9f2fc54305bb3a5f311f1e224c9
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2022-06-23 20:57:04 -0300

    Adicionando anotações nas propriedades

--- Models/Compromisso.cs
+++ Models/Compromisso.cs
@@ -1,6 +1,7 @@
 using Dapper.Contrib.Extensions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CompAPI.Models
 {
@@ -9,10 +10,20 @@ namespace CompAPI.Models
     public class Compromisso
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage="Escolha o tipo de compromisso")]
         public int TipoCompromisso { get; set; }
+
+        [Required(ErrorMessage="O campo descrição é obrigatório")]
+        [MaxLength(30, ErrorMessage="O campo descrição deve conter até 30 caracteres")]
+        [MinLength(10, ErrorMessage="O campo descrição deve conter até 10 caracteres")]
         public string Descricao { get; set; }
+        
         public string Localizacao { get; set; }
+        
+        [Required(ErrorMessage="A data de início é obrigatória")]
         public DateTime DataInicio { get; set; }
+        
         public DateTime DataTermino { get; set; }
         public bool Visivel { get; set; }
         public int ParticipanteId { get; set; }
