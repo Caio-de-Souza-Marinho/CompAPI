aff77f7ea2de66776a251ec668e77b7ebd3d9243
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-06-26 20:54:48 -0300

    Adicionando método get all na controller de compromissos

--- Controllers/CompromissosController.cs
+++ Controllers/CompromissosController.cs
@@ -1,5 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CompAPI.Models;
+using System.Data;
+using CompAPI.DAL;
+using System.Text;
+using Dapper;
+using System.Linq;
+using System;
 
 namespace CompAPI.Controllers
 {
@@ -14,5 +23,39 @@ namespace CompAPI.Controllers
         {
             _config = config;
         }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllAsync()
+        {
+            using (IDbConnection conexao = ConnectionFactory.GetStringConexao(_config))
+            {
+                conexao.Open();
+
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT ID as Id, ID_TIPO_COMPROMISSO as TipoCompromisso, TX_DESCRICAO as Descricao, TX_LOCALIZACAO as Localizacao, ");
+                sql.Append("DT_INICIO as DataInicio, DT_TERMINO as DataTermino, FL_VISIVEL as Visivel ");
+                sql.Append("FROM TB_COMPROMISSO");
+
+                List<Compromisso> lista = (await conexao.QueryAsync<Compromisso>(sql.ToString())).ToList();
+            
+                return Ok(lista);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
