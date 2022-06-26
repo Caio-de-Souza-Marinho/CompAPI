2c51e0bf02d66fb5c91c3e12732d78fbd9bfe9d8
Caio de Souza Marinho <caiosouzamarinho@gmail.com>
2022-06-26 20:57:45 -0300

    Adicionando método get by id na controller de compromissos

--- Controllers/CompromissosController.cs
+++ Controllers/CompromissosController.cs
@@ -37,11 +37,31 @@ namespace CompAPI.Controllers
                 sql.Append("FROM TB_COMPROMISSO");
 
                 List<Compromisso> lista = (await conexao.QueryAsync<Compromisso>(sql.ToString())).ToList();
-            
+
                 return Ok(lista);
             }
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetByIdAsync(int id)
+        {
+            Compromisso c = null;
+            using (IDbConnection conexao = ConnectionFactory.GetStringConexao(_config))
+            {
+                conexao.Open();
+                StringBuilder sql = new StringBuilder();
+                sql.Append("SELECT ID as Id, ID_TIPO_COMPROMISSO as TipoCompromissoId, TX_DESCRICAO as Descricao, TX_LOCALIZACAO as Localizacao, ");
+                sql.Append("DT_INICIO as DataInicio, DT_TERMINO as DataTermino, FL_VISIVEL as Visivel ");
+                sql.Append("FROM TB_COMPROMISSO WHERE ID = @Id ");
+
+                c = await conexao.QueryFirstOrDefaultAsync<Compromisso>(sql.ToString(), new { Id = id });
+
+                if (c != null)
+                    return Ok(c);
+                else
+                    return NotFound("Compromisso não encontrado.");
+            }
+        }
 
 
 
